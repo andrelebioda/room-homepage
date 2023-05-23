f2c9f1fec52838fa876cb45fefc7d89cd03ec19f
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2023-05-24 01:54:21 +0200

    added mobile styles and images // show right content for each slide

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,52 @@
 import "../scss/style.scss";
 
 const imageWrapper = document.querySelector(".image-wrapper");
-const imageSlides = Array.from(
-  document.querySelectorAll(".image-wrapper .image")
-);
+const imageSlides = [...document.querySelectorAll(".image-wrapper .image")];
+const contenBoxes = [...document.querySelectorAll(".content-box")];
 const prevButton = document.getElementById("slide-prev");
 const nextButton = document.getElementById("slide-next");
 
-// imageWrapper.style.width = `${imageWrapper.clientWidth * imageSlides.length}px`;
+let activeSlide = 1;
+let slideWidth;
 
-let activeSlide = 0;
+function initSlider() {
+  slideWidth = imageWrapper.clientWidth;
+  imageSlides.forEach((slide) => (slide.style.minWidth = `${slideWidth}px`));
 
-nextButton.addEventListener("click", () => slider("next"));
-prevButton.addEventListener("click", () => slider("prev"));
-
-function slider(direction) {
-  let slideWidth = imageWrapper.clientWidth;
-
-  if (direction == "next") activeSlide += 1;
-  if (direction == "prev") activeSlide -= 1;
-
-  if (activeSlide == imageSlides.length) activeSlide = 0;
-  if (activeSlide == -1) activeSlide = imageSlides.length - 1;
-
-  imageWrapper.style.transform = `translateX(-${slideWidth * activeSlide}px)`;
+  showContent(activeSlide);
 }
+initSlider();
+
+function slideNext() {
+  activeSlide += 1;
+  if (activeSlide > imageSlides.length) activeSlide = 1;
+
+  imageWrapper.style.transform = `translateX(-${
+    slideWidth * (activeSlide - 1)
+  }px)`;
+  showContent(activeSlide);
+}
+
+function slidePrev() {
+  activeSlide -= 1;
+  if (activeSlide < 1) activeSlide = imageSlides.length;
+
+  imageWrapper.style.transform = `translateX(-${
+    slideWidth * (activeSlide - 1)
+  }px)`;
+  showContent(activeSlide);
+}
+
+function showContent(activeSlide) {
+  contenBoxes.forEach((box, idx) => {
+    activeSlide == idx + 1
+      ? box.classList.add("active")
+      : box.classList.remove("active");
+  });
+}
+
+nextButton.addEventListener("click", slideNext);
+prevButton.addEventListener("click", slidePrev);
+
+// resize window & init slider again
+window.onresize = () => initSlider();
